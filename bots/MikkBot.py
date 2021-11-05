f86efb9d426057a0b606452a9d7cf477d631577c
Mikkel Kofoed <mikk@netcompany.com>
2021-11-05 18:29:01 +0100

    initial bot

--- bots/MikkBot.py
+++ bots/MikkBot.py
@@ -31,14 +31,19 @@ class MikkBot(BotInterface):
         call_check_weight = 1
         fold_weight = 1
 
+        i_start = observation.myPosition == 0
+        opponent_start = observation.myPosition == 1
+
         initial_hand_percent = utils.handValue.getHandPercent(observation.myHand)[0]
         hand_type = utils.handValue.getHandType(observation.myHand)
         board_type = utils.handValue.getBoardHandType(observation.boardCards)
         hand_and_board_type = utils.handValue.getHandType(observation.myHand, observation.boardCards)
 
         # INITIAL CARDS
-        if initial_hand_percent > 0.8:
-            raise_weight += 10
+        if initial_hand_percent > 0.95:
+            raise_weight += 100
+        elif initial_hand_percent > 0.8:
+            raise_weight += 20
         elif initial_hand_percent > 0.6:
             raise_weight += 5
             call_check_weight += 5
@@ -49,6 +54,8 @@ class MikkBot(BotInterface):
             fold_weight += 10
 
         # OPPONENT HISTORY
+        opponent_actions_this_round = observation.get_opponent_history_current_stage()
+        opponent_last_action = opponent_actions_this_round[-1] if len(opponent_actions_this_round) > 0 else None
         opponent_history = observation.get_opponent_history_current_stage()
         opponent_raise_percent = 0
         opponent_fold_percent = 0
@@ -59,14 +66,48 @@ class MikkBot(BotInterface):
             opponent_check_call_percent = (opponent_history.count(Action.CALL) + opponent_history.count(
                 Action.CHECK)) / len(opponent_history)
 
+        if opponent_start:
+            if observation.stage.PREFLOP:
+                if opponent_last_action == Action.RAISE:
+                    if opponent_raise_percent > 0.8:
+                        raise_weight += 10
+                        call_check_weight += 10
+                    elif opponent_raise_percent > 0.2:
+                        raise_weight += 2
+                        call_check_weight += 2
+                    else:
+                        fold_weight += 10
+                if opponent_last_action == Action.FOLD:
+                    if opponent_fold_percent > 0.7:
+                        raise_weight += 10
+                    elif opponent_fold_percent > 0.5:
+                        raise_weight += 5
+                        call_check_weight += 5
+                    else:
+                        fold_weight += 10
+
+            # SUIT
+            suits = utils.handValue.getHighestSuitCount(observation.myHand, observation.boardCards)
+            if suits[0] == 5:
+                return Action.RAISE
+            if suits[0] == 4:
+                raise_weight += 10
+                call_check_weight += 5
+                fold_weight -= 10
+
+            strait = utils.handValue.getLongestStraight(observation.myHand, observation.boardCards)
+            # STRAIT
+            if strait[0] == 5:
+                return Action.RAISE
+            if strait[0] == 4:
+                raise_weight += 10
+                call_check_weight += 5
+                fold_weight -= 10
+
+
+
         choice = random.choices([Action.RAISE, Action.CALL, Action.FOLD], [raise_weight, call_check_weight, fold_weight], k=1)
 
-        # CLOSE TO SUIT
-        #suits = utils.handValue.getHighestSuitCount(observation.myHand, observation.boardCards)
-        #if suits[0] == 5:
-        #    return Action.RAISE
-        #if suits[0] == 4:
-        #    return Action.RAISE
 
         return choice[0]
 
